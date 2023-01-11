d523d61608cde8f5f509afd5bd85ea5ced9f0b43
Lauren Hong <114040016+LH-eliza@users.noreply.github.com>
2023-01-10 20:14:17 -0500

    Update insta_following.py

--- insta_following.py
+++ insta_following.py
@@ -5,3 +5,5 @@ with open('followers.json') as file:
 with open('following.json') as file:
     following_json = json.load(file)
 
+with open('pending_follow_request.json') as file: 
+    pending_follow_request.json = json.load(file)
